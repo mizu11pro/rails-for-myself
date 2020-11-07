8ac5ad2763e261f9ba016f3498ada99fc3b07198
mizu11pro <mizuakasa@gmail.com>
2020-11-07 07:45:43 +0000

    [finish] 0-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,7 +9,25 @@ class TodolistsController < ApplicationController
     #データをデータベースに保存するためのsaveメソッド
     list.save
     #トップ画面へのリダイレクト
-    redirect_to '/top'
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
   private
